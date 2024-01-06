b0ce0bfafaf247b5b3c014bcc5168fe109e3c361
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-01-06 15:59:11 +0200

    data for discover page

---
+++ src/components/discover/data.ts
@@ -0,0 +1,53 @@
+import img1 from "../../assets/discover/1.webp";
+import img2 from "../../assets/discover/2.webp";
+import img3 from "../../assets/discover/3.webp";
+import img4 from "../../assets/discover/4.webp";
+import img5 from "../../assets/discover/5.webp";
+import img6 from "../../assets/discover/6.webp";
+
+interface Discover {
+  title: String;
+  price: number;
+  desc: String;
+  img: string;
+}
+
+const contentArray: Discover[] = [
+  {
+    title: "Happy Anniversary!",
+    price: 2500,
+    desc: "We honor the timeless bond between couples. Whether you're marking a milestone first year or a golden fifty years together, Let us make it special for you.",
+    img: img1,
+  },
+  {
+    title: "Birthday Party",
+    price: 500,
+    desc: "Whether you're planning for yourself or a loved one, we're here to guide you in creating magical moments that will be cherished for years to come. ",
+    img: img2,
+  },
+  {
+    title: "Your Dream Wedding",
+    price: 8500,
+    desc: "We understand that your wedding party is a celebration of love, unity, and cherished moments. We're here to make an unforgettable, momentous event. ",
+    img: img3,
+  },
+  {
+    title: "Graduation Party",
+    price: 1000,
+    desc: "Whether you're celebrating a high school graduation, or an advanced degree, let us help you plan an extraordinary party that honors your success. ",
+    img: img4,
+  },
+  {
+    title: "Gender Reveal Party",
+    price: 750,
+    desc: "From planning tips to capturing the priceless moment, let's make it an occasion filled with joy, anticipation, and heartfelt memories that you'll cherish for a lifetime. ",
+    img: img5,
+  },
+  {
+    title: "Company Occasion",
+    price: 3500,
+    desc: "Let us reflect your company's success and aspirations. Whether it's a milestone celebration or an annual gathering, let us be your partner in making a good event. ",
+    img: img6,
+  },
+];
+export default contentArray;
