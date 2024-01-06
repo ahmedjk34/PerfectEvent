3cbaab2fa6d9dcb6be39efbdacf5ecb3d899d75d
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-01-06 15:59:25 +0200

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Discover from "./components/discover/Discover";
 import Footer from "./components/Footer";
 import Home from "./components/home/Home";
 import Nav from "./components/Nav";
@@ -11,6 +12,7 @@ function App({}: Props) {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
       </Routes>
       <Footer />
     </>
