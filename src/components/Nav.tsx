4d7619cf78f22a113dc9e8dbc7be30c27f79c0e4
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-01-06 15:59:30 +0200

    navigation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,16 @@
 type Props = {};
+import { Link } from "react-router-dom";
 import styles from "../styles/pages/nav.module.scss";
 function Nav({}: Props) {
   return (
     <div className={styles.nav}>
-      <h3>Home</h3>
+      <h3>
+        <Link to="/">Home</Link>
+      </h3>
       <h1>Perfect Event</h1>
-      <h3>Book an Appointment</h3>
+      <h3>
+        <Link to="/discover">Book an Appointment</Link>
+      </h3>
     </div>
   );
 }
