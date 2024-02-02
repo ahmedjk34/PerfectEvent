9834ef248b4a4234777549f50240e00b0efcf5d8
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-02-02 20:30:22 +0200

    added id field

--- src/components/discover/data.ts
+++ src/components/discover/data.ts
@@ -6,6 +6,7 @@ import img5 from "../../assets/discover/5.webp";
 import img6 from "../../assets/discover/6.webp";
 
 interface Discover {
+  id: number;
   title: string;
   price: number;
   desc: string;
@@ -14,36 +15,42 @@ interface Discover {
 
 const contentArray: Discover[] = [
   {
+    id: 1,
     title: "Happy Anniversary!",
     price: 2500,
     desc: "We honor the timeless bond between couples. Whether you're marking a milestone first year or a golden fifty years together, Let us make it special for you.",
     img: img1,
   },
   {
+    id: 2,
     title: "Birthday Party",
     price: 500,
     desc: "Whether you're planning for yourself or a loved one, we're here to guide you in creating magical moments that will be cherished for years to come. ",
     img: img2,
   },
   {
+    id: 3,
     title: "Your Dream Wedding",
     price: 8500,
     desc: "We understand that your wedding party is a celebration of love, unity, and cherished moments. We're here to make an unforgettable, momentous event. ",
     img: img3,
   },
   {
+    id: 4,
     title: "Graduation Party",
     price: 1000,
     desc: "Whether you're celebrating a high school graduation, or an advanced degree, let us help you plan an extraordinary party that honors your success. ",
     img: img4,
   },
   {
+    id: 5,
     title: "Gender Reveal Party",
     price: 750,
     desc: "From planning tips to capturing the priceless moment, let's make it an occasion filled with joy, anticipation, and heartfelt memories that you'll cherish for a lifetime. ",
     img: img5,
   },
   {
+    id: 6,
     title: "Company Occasion",
     price: 3500,
     desc: "Let us reflect your company's success and aspirations. Whether it's a milestone celebration or an annual gathering, let us be your partner in making a good event. ",
