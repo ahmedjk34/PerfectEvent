6f6f75d265973424858fe1472d82ab89a6535807
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-02-06 15:19:36 +0200

    added data for choices

--- src/components/event-page/data.ts
+++ src/components/event-page/data.ts
@@ -4,6 +4,21 @@ import img3 from "../../assets/event/3.webp";
 import img4 from "../../assets/event/4.webp";
 import img5 from "../../assets/event/5.webp";
 import img6 from "../../assets/event/6.webp";
+import tImg1 from "../../assets/event/t1.webp";
+import tImg2 from "../../assets/event/t2.webp";
+import tImg3 from "../../assets/event/t3.webp";
+import vImg1 from "../../assets/event/v1.webp";
+import vImg2 from "../../assets/event/v2.webp";
+import vImg3 from "../../assets/event/v3.webp";
+import cImg1 from "../../assets/event/c1.webp";
+import cImg2 from "../../assets/event/c2.webp";
+import cImg3 from "../../assets/event/c3.webp";
+import flImg1 from "../../assets/event/fl1.webp";
+import flImg2 from "../../assets/event/fl2.webp";
+import flImg3 from "../../assets/event/fl3.webp";
+import foImg1 from "../../assets/event/fo1.webp";
+import foImg2 from "../../assets/event/fo2.webp";
+import foImg3 from "../../assets/event/fo3.webp";
 
 interface Content {
   id: number;
@@ -11,7 +26,11 @@ interface Content {
   title: string;
   our_view: string;
 }
-
+interface Option {
+  title: string;
+  img: string;
+  price: string;
+}
 const eventContentArray: Content[] = [
   {
     id: 1,
@@ -56,5 +75,107 @@ const eventContentArray: Content[] = [
       "We are passionate about transforming ordinary occasions into extraordinary memories that last a lifetime. With a team of dedicated event planners and creative minds, we specialize in curating and orchestrating unforgettable experiences for every milestone in your life. At Perfect Event, we don't just plan events; we create magical moments that are etched in your heart forever. Trust us to make your special occasions truly exceptional. ",
   },
 ];
+const optionsArray: { mainTitle: string; options: Option[] }[] = [
+  {
+    mainTitle: "Theme",
+    options: [
+      {
+        title: "Black-Tie",
+        img: tImg1,
+        price: "450$",
+      },
+      {
+        title: "80's Night",
+        img: tImg2,
+        price: "220$",
+      },
+      {
+        title: "Winter Vibes",
+        img: tImg3,
+        price: "550$",
+      },
+    ],
+  },
+  {
+    mainTitle: "Venue",
+    options: [
+      {
+        title: "Outside Garden",
+        img: vImg1,
+        price: "1200$",
+      },
+      {
+        title: "April's Venue",
+        img: vImg2,
+        price: "4000$",
+      },
+      {
+        title: "A Forest",
+        img: vImg3,
+        price: "850$",
+      },
+    ],
+  },
+  {
+    mainTitle: "Chairs",
+    options: [
+      {
+        title: "Counter-stool",
+        img: cImg1,
+        price: "4₪/Chair",
+      },
+      {
+        title: "Eames Chair",
+        img: cImg2,
+        price: "9₪/Chair",
+      },
+      {
+        title: "Barrel Chair",
+        img: cImg3,
+        price: "15₪/Chair",
+      },
+    ],
+  },
+  {
+    mainTitle: "Flowers",
+    options: [
+      {
+        title: "Lily Flowers",
+        img: flImg1,
+        price: "12₪/Pack",
+      },
+      {
+        title: "Roses",
+        img: flImg2,
+        price: "15₪/Pack",
+      },
+      {
+        title: "Tulips",
+        img: flImg3,
+        price: "20₪/Chair",
+      },
+    ],
+  },
+  {
+    mainTitle: "Food",
+    options: [
+      {
+        title: "Seafood",
+        img: foImg1,
+        price: "40₪/Dish",
+      },
+      {
+        title: "Vegan Food ",
+        img: foImg2,
+        price: "15₪/Dish",
+      },
+      {
+        title: "German Desserts",
+        img: foImg3,
+        price: "2₪/Dish",
+      },
+    ],
+  },
+];
 export type { Content };
-export default eventContentArray;
+export { eventContentArray, optionsArray };
